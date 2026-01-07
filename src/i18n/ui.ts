141dbe0233447fa98d36ea0895fedfd73a68f034
JPBuildsCode <juanp4blog@gmail.com>
2026-01-06 18:55:56 -0600

    first commit

---
+++ src/i18n/ui.ts
@@ -0,0 +1,106 @@
+export const languagesList = {
+  es: "Español",
+  en: "English"
+}
+
+export const labels = {
+  es: {
+    "nav.home": "Inicio",
+    "nav.about": "Acerca de mi",
+    "nav.projects": "Proyectos",
+    "nav.services": "Servicios",
+    "nav.contact": "Contacto",
+    // ------------------------------------------------
+
+    "header.p1": "Hola, soy",
+    "header.p2": "Desarrollador web de 20 años apasionado por la tecnología y crear experiencias digitales modernas",
+    "header.p3": "Autodidacta, curioso y comprometido con el aprendizaje constante. Transformo ideas en interfaces claras y funcionales",
+    "header.cta": "Descargar CV",
+    // ------------------------------------------------
+
+    "services.title": "Mis servicios",
+    // -----
+    "services.title-software": "Desarrollo web",
+    "services.software-service1-title": "Diseño y desarrollo de Landing Pages:",
+    "services.software-service1-description": "Páginas de aterrizaje diseñadas y optimizadas para tus clientes.",
+
+    "services.software-service2-title": "Creación de páginas web informativas:",
+    "services.software-service2-description": "Sitios que presentan a tu marca,negocio, servicio o proyecto de forma clara y profesional.",
+
+    "services.software-service3-title": "Diseño responsivo:",
+    "services.software-service3-description": "Adaptación perfecta a cualquier dispositivo (teléfonos, tablets, computadoras).",
+
+    "services.software-service4-title": "Experiencia de usuario (UX) intuitiva:",
+    "services.software-service4-description": "Navegación sencilla y atractiva para tus visitantes .",
+
+    // -------------------
+
+
+    "services.title-design": "Diseño gráfico",
+    // -----
+    "services.design-service1-title": "Diseño de materiales promocionales:",
+    "services.design-service1-description": "Posters y flyers que captan la atención de tu público.",
+
+    "services.design-service2-title": "Desarrollo de gráficos para eventos:",
+    "services.design-service2-description": "Desde anuncios hasta programas visuales.",
+
+    "services.design-service3-title": "Creación de piezas gráficas para campañas:",
+    "services.design-service3-description": "Adaptables para medios impresos o digitales.",
+
+    "services.design-service4-title": "Comunicación visual clara y atractiva:",
+    "services.design-service4-description": "Para que tu mensaje destaque.",
+
+    // ------------------------------------------------
+
+    "experience.title": "Experiencia"
+  },
+  en: {
+    "nav.home": "Home",
+    "nav.about": "About me",
+    "nav.projects": "Projects",
+    "nav.services": "Services",
+    "nav.contact": "Contact",
+    // ------------------------------------------------
+
+    "header.p1": "Hi, i'm",
+    "header.p2": "20-year-old web developer passionate about technology and creating modern digital experiences",
+    "header.p3": "Self-taught, curious, and committed to constant learning. I turn ideas into clear and functional interfaces.",
+    "header.cta": "Download CV",
+    // ------------------------------------------------
+    "services.title": "My services",
+    // -----
+    "services.title-software": "Web development",
+    "services.software-service1-title": "Landing Page design and development:",
+    "services.software-service1-description": "Landing pages designed and optimized for your customers.",
+
+    "services.software-service2-title": "Creation of informative web pages:",
+    "services.software-service2-description": "Sites that present your brand, business, service or project in a clear and professional way.",
+
+    "services.software-service3-title": "Responsive design:",
+    "services.software-service3-description": "Perfect adaptation to any device (phones, tablets, computers).",
+
+    "services.software-service4-title": "Intuitive user experience (UX):",
+    "services.software-service4-description": "Simple and attractive navigation for your visitors .",
+
+    // -------------------
+
+
+    "services.title-design": "Graphic Design",
+    // -----
+    "services.design-service1-title": "Design of promotional materials:",
+    "services.design-service1-description": "Posters and flyers that capture the attention of your audience.",
+
+    "services.design-service2-title": "Event graphics development:",
+    "services.design-service2-description": "From ads to visual programs.",
+
+    "services.design-service3-title": "Creation of graphic pieces for campaigns:",
+    "services.design-service3-description": "Suitable for print or digital media.",
+
+    "services.design-service4-title": "Clear and attractive visual communication:",
+    "services.design-service4-description": "To make your message stand out.",
+
+    // ------------------------------------------------
+
+    "experience.title": "Experience"
+  },
+}
